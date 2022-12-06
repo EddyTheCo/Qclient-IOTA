358d83a1450f05f7722ee1fc8b02d2036a3c3830
EddyGlezz <eddyglezz95@gmail.com>
2022-12-06 09:33:51 +0100

    added transaction test

--- tests/full_block_with_transaction_payload.cpp
+++ tests/full_block_with_transaction_payload.cpp
@@ -23,14 +23,23 @@ int main(int argc, char** argv)
     QCoreApplication a(argc, argv);
     auto iota=new Client(QUrl("https://api.testnet.shimmer.network"));
 
-    auto seed=QByteArray::fromHex("000102030405060708090a0b0c0d0e0f");
+    auto seed=QByteArray::fromHex("ef4593558d0c3ed9e3f7a2de766d33093cd72372c800fa47ab5765c43ca006b5");
 
-    QVector<quint32> path={44,4219,0,0,1};
+    QVector<quint32> path={44,4219,0,0,0};
     auto MK=Master_key(seed);
     auto keys=MK.slip10_key_from_path(path);
 
-    auto edkeys=qed25519::create_keypair(keys.secret_key());
-    auto pub=edkeys.first;
+    const auto edkeys=qed25519::create_keypair(keys.secret_key());
+    QByteArray publ=QCryptographicHash::hash(edkeys.first,QCryptographicHash::Blake2b_256);
+
+    QVector<quint32> path2={44,4219,0,0,1};
+    auto MK2=Master_key(seed);
+    auto keys2=MK.slip10_key_from_path(path2);
+
+    const auto edkeys2=qed25519::create_keypair(keys2.secret_key());
+    QByteArray publ2=QCryptographicHash::hash(edkeys2.first,QCryptographicHash::Blake2b_256);
+
+    auto pub=publ;
     pub.push_front('\x00');
     qDebug()<<pub.toHex();
 
@@ -76,75 +85,62 @@ int main(int argc, char** argv)
                 auto input=new UTXO_Input(transid,outputIndex);
                 c_array prevOutputSer;
                 prevOutput->serialize(*prevOutputSer.get_buffer());
-                auto Inputs_Commitment=QCryptographicHash::hash(prevOutputSer, QCryptographicHash::Blake2b_256);
+                auto Inputs_Commitment1=QCryptographicHash::hash(prevOutputSer, QCryptographicHash::Blake2b_256);
 
+                const auto Inputs_Commitment=QCryptographicHash::hash(Inputs_Commitment1, QCryptographicHash::Blake2b_256);
 
-                auto eddAddr=new Ed25519_Address(c_array(edkeys.first.constData(),edkeys.first.size()));
-                //auto sendFea=new Sender_Feature(eddAddr);
-                //auto metFea=new Metadata_Feature(dataF("data from IOTA-QT"));
+                auto eddAddr=new Ed25519_Address(c_array(publ));
+
+                auto sendFea=new Sender_Feature(eddAddr);
+                //auto tagFea=new Tag_Feature(fl_array<quint8>("tag from IOTA-QT"));
+                //auto metFea=new Metadata_Feature(fl_array<quint16>("data from IOTA-QT"));
                 auto addUnlcon=new Address_Unlock_Condition(eddAddr);
 
-                auto BaOut=new Basic_Output(1000000000,{addUnlcon},{},{});
+                auto BaOut=new Basic_Output(1000000000,{addUnlcon},{sendFea},{});
 
 
-                auto essence=new Transaction_Essence(networkId,{input},c_array(Inputs_Commitment.constData(),
-                                                                               Inputs_Commitment.size()),{BaOut},nullptr);
-
-
+                auto essence=new Transaction_Essence(networkId,{input},c_array(Inputs_Commitment),{BaOut},nullptr);
 
                 c_array serializedEssence;
                 essence->serialize(*serializedEssence.get_buffer());
-                auto essence_hash=QCryptographicHash::hash(serializedEssence, QCryptographicHash::Blake2b_256);
-                auto sign_t=qed25519::sign(edkeys,essence_hash);
 
-                auto signature_t=new Ed25519_Signature(public_key(edkeys.first.constData(),edkeys.first.size()),
-                                                       signature(sign_t.constData(),sign_t.size()));
+                auto essence_hash=QCryptographicHash::hash(serializedEssence, QCryptographicHash::Blake2b_256);
+                const auto sign_t=qed25519::sign(edkeys,essence_hash);
+
+                auto signature_t=new Ed25519_Signature(public_key(edkeys.first),
+                                                       signature(sign_t));
 
                 auto Sigunlock=new Signature_Unlock(signature_t);
 
                 auto trpay=new Transaction_Payload(essence,{Sigunlock});
-                qDebug()<<"payload:"<<trpay->get_Json();
+
 
                 auto block_= new Block(trpay);
-                qDebug()<<"block:"<<block_->get_Json();
+
                 block_->set_pv(pv);
-                qDebug()<<"block after pv:"<<block_->get_Json();
+
                 auto parents=iota->get_api_core_v2_tips();
 
                 QObject::connect(parents,&Response::returned,parents,[=](QJsonValue data ){
                     auto tips=(data["tips"].toArray());
-                    qDebug()<<"tips:"<<tips;
+
                     std::vector<block_id> parents;
                     for(auto v:tips)parents.push_back(block_id(v));
                     block_->set_parents(parents);
 
-                     qDebug().noquote()<<"block:\n"<<QString(QJsonDocument(block_->get_Json()).toJson(QJsonDocument::Indented));
+
 
                     c_array serialized_block;
                     (*serialized_block.get_buffer())<(*block_);
-                    qDebug()<<"block:"<<serialized_block.toHexString();
-
-                    c_array serilapay;
-                    trpay->serialize(*serilapay.get_buffer());
-                    qDebug()<<"payload:"<<serilapay.toHexString();
-                    qDebug()<<"payload sice:"<<serilapay.size();
-
-                    c_array serialessen;
-                    essence->serialize(*serialessen.get_buffer());
-                    qDebug()<<"essence:"<<serialessen.toHexString();
-                    qDebug()<<"essence sice:"<<serialessen.size();
 
 
                     auto nfinder=new nonceFinder();
                     nfinder->calculate(serialized_block,minPowScore);
 
                     QObject::connect(nfinder,&nonceFinder::nonce_found,nfinder,[=](const quint64 &s){
-                        qDebug()<<"nonce:"<<s;
+
                         block_->set_nonce(s);
                         qDebug().noquote()<<"block:\n"<<QString(QJsonDocument(block_->get_Json()).toJson(QJsonDocument::Indented));
-                        c_array serialized_block;
-                        (*serialized_block.get_buffer())<(*block_);
-                        qDebug()<<serialized_block.toHexString();
 
                         auto res=iota->post_api_core_v2_blocks(block_->get_Json());
 
