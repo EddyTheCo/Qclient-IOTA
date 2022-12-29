e10b0a3c0697b593273bd59092c247a2bbe1e760
EddyGlezz <eddyglezz95@gmail.com>
2022-12-29 17:12:47 +0100

    added a single node output class from json response

--- include/client/qnode_outputs.hpp
+++ include/client/qnode_outputs.hpp
@@ -5,6 +5,16 @@
 #include <QJsonValue>
 namespace qiota{
 
+class Node_output
+{
+public:
+    Node_output(QJsonValue data);
+    std::shared_ptr<qblocks::Output> output(void)const{return out_;}
+    qblocks::Output_Metadata_Response metadata(void)const{return metadata_;}
+private:
+    std::shared_ptr<qblocks::Output> out_;
+    qblocks::Output_Metadata_Response metadata_;
+};
 
 class Node_outputs : public QObject
 {
