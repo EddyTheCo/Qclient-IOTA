f1ce179a67a41b265e657641af8779840506d0cc
EddyGlezz <eddyglezz95@gmail.com>
2023-05-30 08:23:11 +0200

    indent code

--- src/qnode_tips.cpp
+++ src/qnode_tips.cpp
@@ -1,18 +1,17 @@
 #include"client/qnode_tips.hpp"
 #include<QJsonObject>
-#include<QDebug>
 namespace qiota{
 
-Node_tips::Node_tips(Response* resp):response_(resp)
-{
-    QObject::connect(resp, &Response::returned,this, &Node_tips::fill);
-}
-void Node_tips::fill(QJsonValue data)
-{
-    auto tips_arr=(data["tips"].toArray());
-    for(auto v:tips_arr)tips.push_back(qblocks::Block_ID(v));
-    emit finished();
-    response_->deleteLater();
+	Node_tips::Node_tips(Response* resp):response_(resp)
+	{
+		QObject::connect(resp, &Response::returned,this, &Node_tips::fill);
+	}
+	void Node_tips::fill(QJsonValue data)
+	{
+		auto tips_arr=(data["tips"].toArray());
+		for(auto v:tips_arr)tips.push_back(qblocks::Block_ID(v));
+		emit finished();
+		response_->deleteLater();
 
-}
+	}
 }
