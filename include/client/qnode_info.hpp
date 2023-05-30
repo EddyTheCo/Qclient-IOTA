6f284afc588a520563c1bb5db9ca0df8a8ce10d9
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-05-30 10:24:57 +0200

    Develop (#15)
    * Build docs (#13)
    
    * use common reusable  workflows
    
    * use common cmakes
    
    * removed unused things
    
    * added api to docs
    
    * compile threaded version also
    
    * indent code
    
    * set different name to mthread package (#14)

--- include/client/qnode_info.hpp
+++ include/client/qnode_info.hpp
@@ -7,27 +7,26 @@
 namespace qiota{
 
 
-class Node_info : public QObject
-{
-    Q_OBJECT
-public:
-    Node_info(Response*);
-    QJsonObject amount_json(const quint64& amount_);
+	class Node_info : public QObject
+	{
+		Q_OBJECT
+		public:
+			Node_info(Response*);
+			QJsonObject amount_json(const quint64& amount_);
 
-    quint64 network_id_;
-    quint8  protocol_version,decimals;
-    quint32 min_pow_score;
-    QString bech32Hrp,unit,subunit;
-    quint64 vByteFactorKey,vByteFactorData,vByteCost;
-    bool isHealthy,pow_feature;
-public slots:
-    void fill(QJsonValue data);
-signals:
-    void finished(void);
-private:
-    Response* response_;
-    QByteArray network_name_;
-};
+			quint64 network_id_;
+			quint8  protocol_version,decimals;
+			quint32 min_pow_score;
+			QString bech32Hrp,unit,subunit;
+			quint64 vByteFactorKey,vByteFactorData,vByteCost;
+			bool isHealthy,pow_feature;
+			void fill(QJsonValue data);
+		signals:
+			void finished(void);
+		private:
+			Response* response_;
+			QByteArray network_name_;
+	};
 
 
 };
