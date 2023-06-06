5ce2241933622b3c5eeff16962fa4cc98a1b3a1a
EddyGlezz <eddyglezz95@gmail.com>
2023-06-06 09:23:24 +0200

    run example aplication after build

--- examples/full_block_with_tagged_data_payload.cpp
+++ examples/full_block_with_tagged_data_payload.cpp
@@ -14,7 +14,6 @@ int main(int argc, char** argv)
     auto iota_client=new Client();
     iota_client->set_node_address(QUrl(argv[1]));
     //https://api.testnet.shimmer.network
-    //ef4593558d0c3ed9e3f7a2de766d33093cd72372c800fa47ab5765c43ca006b5
 
     if(argc>1)iota_client->set_jwt(argv[2]);
 
