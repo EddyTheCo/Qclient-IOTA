ebb5a71af3ed7136d2098f9f40d6b705ecf9c3ed
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-21 11:49:36 +0100

    removed amount to json because that is now on address wallet

--- src/qnode_info.cpp
+++ src/qnode_info.cpp
@@ -43,28 +43,6 @@ void Node_info::fill(QJsonValue data)
     response_->deleteLater();
 }
 
-QJsonObject Node_info::amount_json(const quint64& amount_)
-{
-    QJsonObject shortValue;
-    shortValue.insert("unit",unit);
-    shortValue.insert("value",QString::number(amount_*1.0/std::pow(10,decimals),'g', 5));
 
-    QJsonObject largeValue;
-    largeValue.insert("unit",subunit);
-    largeValue.insert("value",QString::number(amount_));
-    QJsonObject var;
-    var.insert("shortValue",shortValue);
-    var.insert("largeValue",largeValue);
-
-    if(amount_>std::pow(10,decimals*0.8))
-    {
-        var.insert("default",0);
-    }
-    else
-    {
-        var.insert("default",1);
-    }
-    return var;
-}
 
 }
