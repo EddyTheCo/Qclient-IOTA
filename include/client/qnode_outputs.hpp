254ff0c31a171c11a3a01ae773aec0d9df23f4f7
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-01-31 18:00:23 +0100

    Improvements (#65)
    * improve the cmake logic if qt is not found
    
    * removed unused

--- include/client/qnode_outputs.hpp
+++ include/client/qnode_outputs.hpp
@@ -17,20 +17,6 @@ namespace qiota{
 				QJsonObject var;
 				var.insert("metadata",metadata_.get_Json());
 				var.insert("output",out_->get_Json());
-
-				switch(out_->type()) {
-					case qblocks::Output::NFT_typ:
-						var.insert("chainAddress",qblocks::NFT_Address(metadata_.outputid_.hash<QCryptographicHash::Blake2b_256>()).get_Json()) ;
-						break;
-					case qblocks::Output::Alias_typ:
-						var.insert("chainAddress",qblocks::Alias_Address(metadata_.outputid_.hash<QCryptographicHash::Blake2b_256>()).get_Json()) ;
-						break;
-					case qblocks::Output::Foundry_typ:
-						var.insert("FoundryId",out_->get_id().toHexString()) ;
-						break;
-					case qblocks::Output::Basic_typ:
-						break;
-				}
 				return var;
             }
 		private:
